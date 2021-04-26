2d1485f751809dbbf915d14484727d771f9fae11
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2021-04-26 08:50:18 -0500

    added login html, in progress

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,8 @@ router.route("/").get((req,res)=>{
     res.render("index")   
 })
 
+router.route("/login").get((req,res)=>{
+    res.render("login",{layout:"mainlogin.handlebars"})   
+})
+
 module.exports = router;
