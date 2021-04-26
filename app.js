c6ece5fd07b5878e52b03adfe33780631b7ded55
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2021-04-26 08:13:05 -0500

    added express handlebars
    Co-Authored-By: casillas-m <56522545+casillas-m@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,36 @@
+// importamos las librerias importantes
+const express = require('express')
+const cors = require('cors')
+const handlebars = require('express-handlebars');
+//require('dotenv').config();
+
+// importamos el router
+const router = require('./src/routes')
+
+// de express nos traemos lo necesario
+const { json, urlencoded } = express
+
+// creamos nuestra app
+const app = express()
+
+// definimos un puerto por el cual escucharemos peticiones
+const PORT = process.env.PORT || 8080
+const HOST = process.env.HOST || "0.0.0.0"
+
+// configuraciones para nuestro server
+app.use(json())
+app.use(urlencoded({ extended: false }))
+const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
+app.use(cors(corsOptions))
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+app.use("/assets", express.static('assets'));
+app.use("/images", express.static('images'));
+
+
+// indicamos que usaremos un router
+app.use(router)
+
+// iniciamos nuestro server
+app.listen(PORT, HOST, () => { console.log(`Server listening on port ${PORT} and host ${HOST}`); })
