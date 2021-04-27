0e58bc8c96b77127ad6f0f9eb59601d4d7d41800
Michel Maris Mora <39529432+MichelMM@users.noreply.github.com>
2021-04-26 19:35:27 -0500

    Sign up created

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,12 @@ router.route("/").get((req,res)=>{
     res.render("index")   
 })
 
-router.route("/login").get((req,res)=>{
-    res.render("login",{layout:"mainlogin.handlebars"})   
+router.route("/signin").get((req,res)=>{
+    res.render("signin",{layout:"mainlogin.handlebars"})   
+})
+
+router.route("/signup").get((req,res)=>{
+    res.render("signup",{layout:"mainlogin.handlebars"})   
 })
 
 module.exports = router;
